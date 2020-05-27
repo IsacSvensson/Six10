c5818d567728a8669e8c92aa7474bf2f3151cf43
IsacSvensson <56951918+IsacSvensson@users.noreply.github.com>
2020-05-27 21:35:37 +0200

    Started working on the type enum that is going to classify the tokens.

--- Source/lexer.cpp
+++ Source/lexer.cpp
@@ -1,5 +1,18 @@
 #include "lexer.hpp"
 
+enum types {
+    LETTER,
+    NUMBER,
+    IDENTIFIER,
+    OPERATOR,
+    NEWLINE,
+    PARENTHESES,
+    TABB,
+    COMMENT,
+    KEYWORD,
+    DOT
+};
+
 std::vector<std::pair<int, std::string>> lexer(std::string sourceCode){
     return;
 }
