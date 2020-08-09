49dd39a0a3e32d251555c75360e34d203b744453
IsacSvensson <56951918+IsacSvensson@users.noreply.github.com>
2020-08-09 23:32:06 +0200

    Finished test of arithmetic ops

--- Source/test.cpp
+++ Source/test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 #include "test.hpp"
 #include "parser.hpp"
 #include "interpreter.hpp"
@@ -24,8 +25,11 @@ bool testCode(std::string code, double expectedResult){
     Context* context = new Context("<program>");
     context->symTab = &symtab;
     auto result = interpreter.visit(res->node, context);
-    if (result->error) 
+    if (result->error){
+        if (expectedResult == -9999)
+            return true;
         return false;
+    }
     if (result->type == INTEGER || result->type == FLOAT)
         return ((Number*)(result->value))->value == expectedResult;
     return false;
@@ -42,10 +46,11 @@ bool testAllFunc(){
 }
 
 bool testArithm(){
+    bool finalSuccess = true;
+    std::cout << "\tTest 1.1 - Basic Operations ('+', '-', '*', '/' and '^'):" << std::endl;;
+    std::string code[5]{"5 + 5", "33 - 10", "3 * 3", "10 / 4", "5 ^ 2"};
+    double expVal[5]{10, 23, 9, 2.5, 25};
     bool success = true;
-    std::cout << "\tTesting basic operations ('+', '-', '*', '/' and '^'):" << std::endl;;
-    std::string code[5]{"5 + 5", "33 - 10", "3 * 3", "10 / 4", "25 ^ 0.5"};
-    double expVal[5]{10, 23, 9, 2.5, 5};
     for (int i = 0; i < 5; i++){
         std::cout << "\t\t" << code[i] << " = " << expVal[i] << ":";
         if (testCode(code[i], expVal[i]))
@@ -53,11 +58,74 @@ bool testArithm(){
         else
         {
             std::cout << " Failed" << std::endl;
-            success = false;
+            finalSuccess = success = false;
         }
     }
-    return success;
+    if (success)
+        std::cout << "\tTest 1.1 - Success\n" << std::endl;
+    else
+        std::cout << "\tTest 1.1 - Fail\n" << std::endl;
+
+    std::cout << "\tTest 1.2 - Multiple Operations:" << std::endl;;
+    std::string codeMO[5]{"5 + 5 - 3", "5*9-15/3", "(3 * 3)*3", "(10 / 4 * 4 + 15) ^ 0.5", "(6*6)/(2*2)"};
+    double expValMO[5] = {7, 40, 27, 5, 9};
+    success = true;
+    for (int i = 0; i < 5; i++){
+        std::cout << "\t\t" << codeMO[i] << " = " << expValMO[i] << ":";
+        if (testCode(codeMO[i], expValMO[i]))
+            std::cout << " Success" << std::endl;
+        else
+        {
+            std::cout << " Failed" << std::endl;
+            finalSuccess = success = false;
+        }
+    }
+    if (success)
+        std::cout << "\tTest 1.2 - Success\n" << std::endl;
+    else
+        std::cout << "\tTest 1.2 - Fail\n" << std::endl;
+
+    std::cout << "\tTest 1.3 - Negative numbers:" << std::endl;;
+    std::string codeNN[5]{"-5 + 5", "-3 * 7", "-255 / -5", "-10 ^ 3", "-1"};
+    double expValNN[5] = {0, -21, 51, -1000, -1};
+    success = true;
+    for (int i = 0; i < 5; i++){
+        std::cout << "\t\t" << codeNN[i] << " = " << expValNN[i] << ":";
+        if (testCode(codeNN[i], expValNN[i]))
+            std::cout << " Success" << std::endl;
+        else
+        {
+            std::cout << " Failed" << std::endl;
+            finalSuccess = success = false;
+        }
+    }
+    if (success)
+        std::cout << "\tTest 1.3 - Success\n" << std::endl;
+    else
+        std::cout << "\tTest 1.3 - Fail\n" << std::endl;
+
+    std::cout << "\tTest 1.4 - Illegal Operations:" << std::endl;;
+    std::string codeIO[2]{"5 / 0", "(10 / (5 - 5)) + 10"};
+    double expValIO[2] = {-9999, -9999};
+    success = true;
+    for (int i = 0; i < 2; i++){
+        std::cout << "\t\t" << codeIO[i] << " = " << "Runtime Error: Division by 0 not allowed" << ":";
+        if (testCode(codeIO[i], expValIO[i]))
+            std::cout << " Success" << std::endl;
+        else
+        {
+            std::cout << " Failed" << std::endl;
+            finalSuccess = success = false;
+        }
+    }
+    if (success)
+        std::cout << "\tTest 1.4 - Success\n" << std::endl;
+    else
+        std::cout << "\tTest 1.4 - Fail\n" << std::endl;
+    return finalSuccess;
 }
+
+
 /* 
 bool testCompareAndLogic(){
 
