6be10f661e10f95a4b701f28d61f5716ee0f8aee
Isac Svensson <isac.m.svensson@gmail.com>
2021-05-07 15:46:40 +0200

    basic shell interpreter for testing

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from sys import argv
+import platform
+from lexer import Lexer
+
+def main():
+    if len(argv) > 1:
+        # Will handle file reading
+        pass
+    else:
+        console_input = None
+        print("Six10 v.0\nRun on [{}]\n-------------".format(platform.system()))
+        while console_input != "":
+            print(">>> ", end="")
+            console_input = input()
+            lexer = Lexer(console_input, "Shell")
+            lexer.make_tokens()
+            print(lexer.tokens)
+
+
+if __name__ == "__main__":
+    main()
