09d3ea7cdc2b72888ee132a92765f7263c66f5fb
Isac Svensson <isac.m.svensson@gmail.com>
2021-05-27 16:24:41 +0200

    broke out filereader and shell-part from main into own mehotds

--- src/main.py
+++ src/main.py
@@ -3,31 +3,36 @@ from sys import argv
 import platform
 from lexer import Lexer
 
+def run_file(filename):
+    source = str()
+
+    with open() as fh:
+        source = fh.read()
+    
+    lexer = Lexer(source, filename)
+    lexer.make_tokens()
+    for token in lexer.tokens:
+        print(token)
+
+def run_shell():
+    console_input = None
+    print("Six10 v.0\nRun on [{}]\n-------------".format(platform.system()))
+    while console_input != "":
+        print(">>> ", end="")
+        console_input = input()
+        lexer = Lexer(console_input, "Shell")
+        lexer.make_tokens()
+        for token in lexer.tokens:
+            print(token)
+
 def main():
     """
     Main method for running the language.
     """
     if len(argv) > 1:
-        source = str()
-
-        print(argv)
-        with open(argv[1]) as fh:
-            source = fh.read()
-        
-        lexer = Lexer(source, argv[1])
-        lexer.make_tokens()
-        for token in lexer.tokens:
-            print(token)
+        run_file(argv[1])
     else:
-        console_input = None
-        print("Six10 v.0\nRun on [{}]\n-------------".format(platform.system()))
-        while console_input != "":
-            print(">>> ", end="")
-            console_input = input()
-            lexer = Lexer(console_input, "Shell")
-            lexer.make_tokens()
-            for token in lexer.tokens:
-                print(token)
+        run_shell()
 
 
 if __name__ == "__main__":
