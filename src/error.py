599fd488d9c1e9fcb43e61d0ac4a094f4e066e38
Isac Svensson <isac.m.svensson@gmail.com>
2021-05-27 14:04:00 +0200

    added method for comparing objects

--- src/error.py
+++ src/error.py
@@ -1,3 +1,8 @@
 class Error:
     def __init__(self, msg):
         self.message = msg
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, Error):
+            return self.message == o.message
+        return False
