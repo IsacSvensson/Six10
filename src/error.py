3e806aa013a26bc44b525ebd09dfa467cc95a105
IsacSvensson <56951918+IsacSvensson@users.noreply.github.com>
2022-01-12 23:03:07 +0100

    added start and end pos instead of a single

--- src/error.py
+++ src/error.py
@@ -1,5 +1,7 @@
 class Error:
-    def __init__(self, msg):
+    def __init__(self, msg, pos_start=None, pos_end=None):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
         self.message = msg
 
     def __eq__(self, o: object) -> bool:
