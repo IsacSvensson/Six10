39eebe6e69345d791396a47981f8f8ca155af979
IsacSvensson <56951918+IsacSvensson@users.noreply.github.com>
2022-01-12 23:06:09 +0100

    name change and diverse changes

--- src/six10_parser.py
+++ src/six10_parser.py
@@ -1,7 +1,7 @@
 from error import Error
 from lex_token import Token
 import terminal_tokens as tt
-from ast_node import Bin_op_node, Unary_op_node, Number_node
+from ast_node import Bin_op_node, Program_node, Unary_op_node, Number_node
 
 class Parse_result:
     def __init__(self):
@@ -39,15 +39,59 @@ class Parser:
             self.current_token = self.tokens[self.token_index]
 
     def parse(self):
-        res = self.expr()
+        res = self.statements()
     
         return res
+
+    def statements(self):
+        res = Parse_result()
+        stmts = []
+        while self.current_token.datatype != tt._EOF:
+            if self.current_token.datatype in tt._CMP_STMT:
+                node = res.register(self.compound_statment())
+            else:
+                node = res.register(self.small_statment())
+            if res.error: return res
+            stmts.append(node)
+            while self.current_token.datatype == tt._NEWLINE:
+                self.advance()
+        res.success(Program_node(self.tokens[0].start.filename, stmts))
+        return res
+
+    def compound_statment(self):
+        raise NotImplementedError
+
+    def small_statment(self):
+        res = Parse_result()
+        tok = self.current_token
+
+        if tok.datatype == tt._RETURN:
+            raise NotImplementedError
+        elif tok.datatype == tt._IMPORT:
+            raise NotImplementedError
+        elif tok.datatype == tt._RAISE:
+            raise NotImplementedError
+        elif tok.datatype == tt._PASS:
+            raise NotImplementedError
+        elif tok.datatype == tt._DELETE:
+            raise NotImplementedError
+        elif tok.datatype == tt._BREAK:
+            raise NotImplementedError
+        elif tok.datatype == tt._CONTINUE:
+            raise NotImplementedError
+        elif tok.datatype == tt._COMMENT:
+            raise NotImplementedError
+        elif tok.datatype == tt._MULTI_COMMENT:
+            raise NotImplementedError
+        else:
+            return self.expr()
+
 
     def expr(self):
         return self.bin_op(self.term, (tt._PLUS, tt._MINUS))
 
     def term(self):
-        return self.bin_op(self.factor, (tt._MULT, tt._DIV))
+        return self.bin_op(self.factor, (tt._MULT, tt._DIV, tt._MOD, tt._FLOOR))
 
     def factor(self):
         res = Parse_result()
@@ -79,9 +123,9 @@ class Parser:
                 res.register(self.advance())
                 return res.success(expr)
             else:
-                return res.failure(Error("Expected ')'"))
+                return res.failure(Error("Expected ')'", tok.start, self.current_token.end))
         
-        return res.failure(Error("Expected int or float"))
+        return res.failure(Error("Expected int or float", self.current_token.start, self.current_token.end))
     
     def bin_op(self, func, ops):
         res = Parse_result()
