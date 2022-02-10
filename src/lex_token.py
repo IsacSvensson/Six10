9e68b30cbaaafdbfeb40818e73aae1963262c7ee
IsacSvensson <56951918+IsacSvensson@users.noreply.github.com>
2022-02-10 22:57:12 +0100

    trying out dataclass  instead of regular class for tokens

--- src/lex_token.py
+++ src/lex_token.py
@@ -1,15 +1,16 @@
+from dataclasses import dataclass
+from position import Position
+
+
+@dataclass
 class Token:
     """
     Class that contain data for tokens.
     """
-    def __init__(self, datatype, value, start, end):
-        """
-        Inits the class with datatype, value and start and end positions.
-        """
-        self.datatype = datatype
-        self.value = value
-        self.start = start
-        self.end = end
+    datatype : str
+    value : str
+    start : Position
+    end : Position
 
     def __str__(self):
         return f"[{self.datatype}]: {self.value}"
